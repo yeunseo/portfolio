debd973e2e551d0147c182e4c54d786fd08565ed
yeunseo <59273807+yeunseo@users.noreply.github.com>
2020-07-18 22:58:38 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+//navbar 메뉴 클릭시 해당 section으로 이동
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth", block: "center" });
+});
