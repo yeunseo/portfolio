b4d85589ce91c46eb986659092bc413d9ce0a41b
yeunseo <59273807+yeunseo@users.noreply.github.com>
2020-07-27 15:56:44 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//스크롤시 navbar 투명화
+// 스크롤시 navbar 투명화
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
@@ -11,7 +11,7 @@ document.addEventListener("scroll", () => {
   }
 });
 
-//navbar 메뉴 클릭시 해당 section으로 이동
+// navbar 메뉴 클릭시 해당 section으로 이동
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
@@ -22,13 +22,13 @@ navbarMenu.addEventListener("click", (event) => {
   scrollIntoView(link);
 });
 
-//Home의 contact me 버튼 클릭시 맨 하단의 contact section으로 이동
+// Home의 contact me 버튼 클릭시 맨 하단의 contact section으로 이동
 const contactMe = document.querySelector(".home__contact");
 contactMe.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
-//스크롤시 Home section 투명화
+// 스크롤시 Home section 투명화
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
@@ -37,6 +37,21 @@ document.addEventListener("scroll", () => {
   }
 });
 
+// 클릭시 페이지 맨 위로 이동하는 Arrow up 버튼 생성
+const arrowUp = document.querySelector(".arrow_up");
+document.addEventListener("click", () => {
+  scrollIntoView("#home");
+});
+// Arrow up 버튼 스크롤시 나타내기
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add("visible");
+  } else {
+    arrowUp.classList.remove("visible");
+  }
+});
+
+// 함수 - 지정한 위치로 스크롤
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth", block: "center" });
